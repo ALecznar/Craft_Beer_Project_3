2241e0f328f0d4926b12848d63d2416a4cf398e0
Aron Lecznar <arondlecznar@gmail.com>
2023-09-26 19:53:55 -0400

    Added Bar Chart

--- main_js.js
+++ main_js.js
@@ -16,7 +16,7 @@ d3.json("michigan_sorted.json").then(function(data) {
         var brewery = data[i];
         if (brewery.latitude && brewery.longitude) {
             // Create a marker at the brewery's location
-            var marker = L.marker([brewery.latitude, brewery.longitude])
+            L.marker([brewery.latitude, brewery.longitude])
                 .bindPopup(
                     "<h5>" + brewery.name + "</h5>" +
                     "<p>City: " + brewery.city + "</p>" +
@@ -26,4 +26,55 @@ d3.json("michigan_sorted.json").then(function(data) {
                 .addTo(myMap);
         }
     }
+
+    // Group the data by city and count the number of breweries
+    var grouped = d3.group(data, d => d.city);
+    var groupedArray = Array.from(grouped).map(d => ({ key: d[0], value: d[1].length }));
+
+    // Sort the data based on the count of breweries
+    var sorted = groupedArray.sort((a, b) => b.value - a.value).slice(0, 10);
+
+    // Create a bar chart using the top 10 cities by brewery count
+    var svgWidth = 900; 
+    var svgHeight = 500;
+
+
+
+    var svg = d3.select("#chartContainer").append("svg")
+                .attr("width", svgWidth)
+                .attr("height", svgHeight)
+                .attr("id", "barChart");
+
+    var xScale = d3.scaleBand()
+                   .domain(sorted.map(d => d.key))
+                   .range([0, svgWidth])
+                   .padding(0.4);
+
+    var yScale = d3.scaleLinear()
+                   .domain([0, d3.max(sorted, d => d.value)])
+                   .range([svgHeight, 0]);
+
+    var xAxis = d3.axisBottom(xScale);
+    var yAxis = d3.axisLeft(yScale);
+
+
+    svg.append("g")
+       .attr("transform", "translate(0," + (svgHeight - 20) + ")")
+       .call(xAxis)
+    
+    svg.append("g")
+       .attr("transform", "translate(50, 0)")
+       .call(yAxis);
+
+    svg.selectAll(".bar")
+       .data(sorted)
+       .enter()
+       .append("rect")
+       .attr("class", "bar")
+       .attr("x", d => xScale(d.key))
+       .attr("y", d => yScale(d.value))
+       .attr("width", xScale.bandwidth())
+       .attr("height", d => svgHeight - yScale(d.value) - 20)
+       .attr("fill", "steelblue");
+       
 });
