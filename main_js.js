954257a884f4ff627304468ddd76749827d25eb8
Aron Lecznar <arondlecznar@gmail.com>
2023-09-28 18:28:48 -0400

    Drop down basic add

--- main_js.js
+++ main_js.js
@@ -1,3 +1,8 @@
+// Margin around the graph
+var margin = {top: 20, right: 20, bottom: 70, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
 // Create a map object
 var myMap = L.map("map", {
     center: [44.3148, -85.6024],  // Center of Michigan
@@ -34,47 +39,67 @@ d3.json("michigan_sorted.json").then(function(data) {
     // Sort the data based on the count of breweries
     var sorted = groupedArray.sort((a, b) => b.value - a.value).slice(0, 10);
 
-    // Create a bar chart using the top 10 cities by brewery count
-    var svgWidth = 900; 
-    var svgHeight = 500;
-
-
-
+    // Create an SVG element and set its dimensions
     var svg = d3.select("#chartContainer").append("svg")
-                .attr("width", svgWidth)
-                .attr("height", svgHeight)
-                .attr("id", "barChart");
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+
+        svg.append("text")
+                .attr("x", width / 2)
+                .attr("y", 0 - (margin.top / 2))
+                .attr("text-anchor", "middle")
+                .style("font-size", "24px")
+                .style("text-decoration", "underline")
+                .text("Michigan Top Ten");
+
+
+
+    // Set the scales
     var xScale = d3.scaleBand()
                    .domain(sorted.map(d => d.key))
-                   .range([0, svgWidth])
+                   .range([0, width])
                    .padding(0.4);
 
     var yScale = d3.scaleLinear()
                    .domain([0, d3.max(sorted, d => d.value)])
-                   .range([svgHeight, 0]);
+                   .range([height, 0]);
 
-    var xAxis = d3.axisBottom(xScale);
-    var yAxis = d3.axisLeft(yScale);
-
+    // Append the axes
+    svg.append("g")
+       .attr("transform", "translate(0," + height + ")")
+       .call(d3.axisBottom(xScale));
 
     svg.append("g")
-       .attr("transform", "translate(0," + (svgHeight - 20) + ")")
-       .call(xAxis)
-    
-    svg.append("g")
-       .attr("transform", "translate(50, 0)")
-       .call(yAxis);
+       .call(d3.axisLeft(yScale));
 
+    svg.append("text")
+       .attr("transform", "rotate(-90)")
+       .attr("y", 0 - margin.left)
+       .attr("x",0 - (height / 2))
+       .attr("dy", "1em")
+       .style("text-anchor", "middle")
+       .text("# Of Breweries");
+
+
+
+
+    // Append the bars
     svg.selectAll(".bar")
        .data(sorted)
-       .enter()
-       .append("rect")
+       .enter().append("rect")
        .attr("class", "bar")
        .attr("x", d => xScale(d.key))
-       .attr("y", d => yScale(d.value))
        .attr("width", xScale.bandwidth())
-       .attr("height", d => svgHeight - yScale(d.value) - 20)
-       .attr("fill", "steelblue");
-       
+       .attr("y", d => yScale(d.value))
+       .attr("height", d => height - yScale(d.value));
+
+    // Add x-axis label
+    svg.append("text")
+       .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.top + 30) + ")")
+       .style("text-anchor", "middle")
+       .text("City");
+
 });
